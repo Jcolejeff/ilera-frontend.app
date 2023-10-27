54fce7b8605eb6daa28f75acf5a0bb828371a0f3
ikwuh jeffery enokela <ikwuhjefferye@gmail.com>
2023-10-27 02:09:52 +0100

    feat : added addmentment modal

--- src/components/modal/Patients/AddAttactment/index.tsx
+++ src/components/modal/Patients/AddAttactment/index.tsx
@@ -15,7 +15,7 @@ const AddAttachmentModal = ({ trigger, triggerClassName, title }: Iprops) => {
   return (
     <Dialog onOpenChange={(i) => setModalOpen(i)} open={modalOpen}>
       <DialogTrigger className={triggerClassName}>{trigger}</DialogTrigger>
-      <DialogContent className='h-screen max-w-full overflow-auto overflow-x-hidden bg-white  px-6 pt-[3rem] sm:w-[65vw] md:h-5/6 md:!max-w-[1000px] lg:px-[2rem]'>
+      <DialogContent className='h-screen max-w-full overflow-auto overflow-x-hidden bg-white  px-6 pt-[3rem] sm:w-[65vw] md:h-5/6 md:!max-w-[700px] lg:px-[2rem]'>
         <div className='flex w-full flex-col '>
           <h4 className='mb-[1.72rem] text-sm font-[500] leading-[28px] tracking-[0.17px] md:text-lg'>
             {title || `Attachment`}
